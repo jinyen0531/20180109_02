e75e2bf88272cec3dba527f909057b71152f3d6e
yen <jinyen0531@gmail.com>
2018-01-10 09:55:26 +0800

    版本不同須注意的地方

--- app/src/main/java/com/yenyu/a20180109_02/MainActivity.java
+++ app/src/main/java/com/yenyu/a20180109_02/MainActivity.java
@@ -29,13 +29,15 @@ public class MainActivity extends AppCompatActivity {
         nm=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         //從系統service取出NOTIFICATION_SERVICE 通知管理
 
-        if(Build.VERSION.SDK_INT >=26) {
+        if(Build.VERSION.SDK_INT >=26) //android8以上才執行
+        {
             regChannel();
         }
 
     }
 
     @TargetApi(Build.VERSION_CODES.O)
+    //只對android8做以下這段程式
     private void regChannel()
     {
         channelLove = new NotificationChannel(
@@ -47,16 +49,16 @@ public class MainActivity extends AppCompatActivity {
         nm.createNotificationChannel(channelLove);
         //建立一個通知頻道();
     }
-    @TargetApi(Build.VERSION_CODES.O)
-    @SuppressWarnings("deprecation")
+    @TargetApi(Build.VERSION_CODES.O) //針對android oreo寫的程式
+    @SuppressWarnings("deprecation") //忽略舊的程式造成的錯誤
     public void click1(View v) //點擊後跳處推撥通知
     {
         Notification.Builder builder;
-        if(Build.VERSION.SDK_INT>= 26)
+        if(Build.VERSION.SDK_INT>= 26) //如果符合android8以上 有channel
         {
             builder =new Notification.Builder(MainActivity.this, idLove);
         }
-        else
+        else // 不到8的版本沒有channel這個參數
         {
             builder =new Notification.Builder(MainActivity.this);
         }
@@ -64,11 +66,18 @@ public class MainActivity extends AppCompatActivity {
         Intent it = new Intent(MainActivity.this,InfoActivity.class);
         it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |Intent.FLAG_ACTIVITY_CLEAR_TOP|
         Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent pi = PendingIntent.getActivity(MainActivity.this,123,
-                PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pi = PendingIntent.getActivity(MainActivity.this, 123,
+                it, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentTitle("測試");
         builder.setContentText("這是內容");
-        builder.setSmallIcon(R.drawable.ic_launcher_foreground); //小圖案
+        if(Build.VERSION.SDK_INT>=26) //android8跟以前的版本放的圖案大小不同，使用上需注意
+        {
+            builder.setSmallIcon(R.drawable.ic_launcher_foreground); //小圖案
+        }
+        else
+        {
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+        }
         builder.setAutoCancel(true); //點了後自動消失
         builder.setContentIntent(pi);
 
